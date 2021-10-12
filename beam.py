51aa7aabddc7b7d44fa1e409f2ed123e35a07f3e
Philipp Dijkstal <philipp.dijkstal@psi.ch>
2021-10-12 19:27:55 +0200

    status

--- beam.py
+++ beam.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 def gen_twoD(outp, n_particles, gemit, beta, alpha, cutoff_sigma, n_mesh):
     gamma = (1 + alpha**2)/beta
     sigma_x = np.sqrt(gemit*beta)
@@ -13,6 +12,7 @@ def gen_twoD(outp, n_particles, gemit, beta, alpha, cutoff_sigma, n_mesh):
     rho = 1/(2*np.pi*gemit) * np.exp(-1/(2*gemit) * (gamma*x_arr**2 + 2*alpha * x_arr * xp_arr + beta*xp_arr**2))
     rho1 = np.reshape(rho, rho.size)
     cum = np.cumsum(rho1) * xp_step * x_step
+    cum /= cum.max()
     randoms = np.random.rand(int(n_particles))
     indices = np.arange(len(cum))
     interp_indices = np.round(np.interp(randoms, cum, indices))
@@ -22,10 +22,14 @@ def gen_twoD(outp, n_particles, gemit, beta, alpha, cutoff_sigma, n_mesh):
     outp[0] = x_arr[x_index] + (np.random.rand(int(n_particles))-0.5) * x_step
     outp[1] = xp_arr0[x_index] + (np.random.rand(int(n_particles))-0.5) * xp_step
 
-def gen_beam(nemitx, nemity, betax, alphax, betay, alphay, p_central, rms_bunch_duration, n_particles, cutoff_sigma=5, n_mesh=200):
+def gen_beam6D(nemitx, nemity, betax, alphax, betay, alphay, p_central, beamProfile, n_particles, cutoff_sigma=5, n_mesh=200):
     outp = np.zeros([6, int(n_particles)])
     gen_twoD(outp[0:2,:], n_particles, nemitx/p_central, betax, alphax, p_central, cutoff_sigma, n_mesh)
     gen_twoD(outp[2:4,:], n_particles, nemity/p_central, betay, alphay, p_central, cutoff_sigma, n_mesh)
+
+    outp[5] = p_central
+
+
 
 
 
@@ -40,7 +44,7 @@ if __name__ == '__main__':
     rms_bunch_duration = 50e-15
     n_particles = 100e3
     cutoff_sigma = 5
-    gen_beam(nemitx, nemity, betax, alphax, betay, alphay, p_central, rms_bunch_duration, n_particles, cutoff_sigma=10)
+    gen_beam6D(nemitx, nemity, betax, alphax, betay, alphay, p_central, rms_bunch_duration, n_particles, cutoff_sima=cutoff_sigma)
 
 
 

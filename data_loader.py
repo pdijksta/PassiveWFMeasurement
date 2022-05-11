816902532aa9d31182c087961a5431172abac4ca
Philipp Dijkstal <philipp.dijkstal@psi.ch>
2022-05-11 17:53:29 +0200

    progress on data classes

--- data_loader.py
+++ data_loader.py
@@ -1,5 +1,6 @@
 import numpy as np
 from . import gaussfit
+from . import h5_storage
 from . import config
 from . import image_analysis
 
@@ -82,6 +83,21 @@ class DataLoaderSinglePosition:
         self.projx = np.sum(self.images, axis=1, dtype=np.float64)
         self.projy = np.sum(self.images, axis=2, dtype=np.float64)
 
+    def noise_cut(self, subtract_quantile, max_quantile):
+        old_images = self.images
+        new_images = np.zeros_like(old_images)
+        for ctr in range(len(new_images)):
+            if subtract_quantile is not None:
+                new_image = old_images[ctr] - np.quantile(old_images[ctr], subtract_quantile)
+            else:
+                new_image = old_images[ctr]
+            if max_quantile is None:
+                max_ = None
+            else:
+                max_ = np.quantile(new_image, max_quantile)
+            new_images[ctr] = np.clip(new_image, 0, max_)
+        self.images = new_images
+
     def get_median_index(self, dimension):
         if dimension in ('X', 'x'):
             return get_median(self.projx, 'mean', 'index')
@@ -90,7 +106,7 @@ class DataLoaderSinglePosition:
 
     def get_median_image(self, dimension, **kwargs):
         index = self.get_median_index(dimension)
-        img = image_analysis.Image(self.images[index], self.x_axis_m, self.y_axis_m, **kwargs)
+        img = image_analysis.Image(self.images[index].astype(np.float64), self.x_axis_m, self.y_axis_m, **kwargs)
         return img
 
 class PSICalibrationData(DataLoaderMultiPosition):
@@ -104,4 +120,18 @@ class PSICalibrationData(DataLoaderMultiPosition):
         position_key = raw_data['streaker'] + ':CENTER'
         assert position_key in meta_data
         self.add_data(positions, images, x_axis_m, y_axis_m, meta_data, screen_name, position_key)
+
+class PSISinglePositionData(DataLoaderSinglePosition):
+    def __init__(self, file_or_dict, screen_name):
+        if type(file_or_dict) is dict:
+            raw_data = file_or_dict
+        else:
+            raw_data = h5_storage.loadH5Recursive(file_or_dict)
+
+        x_axis_m = raw_data['pyscan_result']['x_axis_m']
+        y_axis_m = raw_data['pyscan_result']['y_axis_m']
+        images = raw_data['pyscan_result']['image']
+        meta_data = raw_data['meta_data_begin']
+
+        DataLoaderSinglePosition.__init__(self, images, x_axis_m, y_axis_m, meta_data, screen_name)
 

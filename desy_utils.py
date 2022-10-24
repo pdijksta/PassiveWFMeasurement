d42977e0679c2dc00dd52c2c21ca2026a9ad79d7
dijkstal <philipp.dijkstal@psi.ch>
2022-10-24 20:06:44 +0200

    improvements

--- desy_utils.py
+++ desy_utils.py
@@ -51,6 +51,7 @@ class Xfel_data(logMsg.LogMsgBase):
             data = filename_or_data
         else:
             data = np.load(filename_or_data)
+        self.raw_data = data
 
         if profile is None:
             crisp_intensity = data['crisp'][1]
@@ -68,12 +69,12 @@ class Xfel_data(logMsg.LogMsgBase):
                 'pyscan_result': {
                     'x_axis_m': x_axis_m,
                     'y_axis_m': y_axis_m,
-                    'image': new_images,
+                    'image': new_images[:,::-1],
                     },
                 'meta_data_begin': {
                     '%s:ENERGY' % beamline: energy_eV/1e6,
                     '%s:GAP' % beamline: gap*1e3,
-                    '%s:CENTER' % beamline: (gap/2-init_distance)*1e3,
+                    '%s:CENTER' % beamline: -(gap/2-init_distance)*1e3,
                     '%s:CHARGE' % beamline: charge*1e12,
                     },
                 }
@@ -103,6 +104,7 @@ class Xfel_data(logMsg.LogMsgBase):
         tracker = self.tracker
         axis, proj, _, index = data_loader.screen_data_to_median(self.data['pyscan_result'], self.tracker.structure.dim)
         trans_dist = self.get_median_sd()
+        trans_dist.smoothen(20e-6)
 
         method0 = tracker.find_beam_position_options['method']
         position_explore0 = tracker.find_beam_position_options['position_explore']
@@ -114,15 +116,28 @@ class Xfel_data(logMsg.LogMsgBase):
             tracker.find_beam_position_options['position_explore'] = position_explore0
             tracker.find_beam_position_options['method'] = method0
 
+        def do_interp(trans_dist, half_int=None):
+            indices, _ = find_peaks(trans_dist.intensity, height=0.2*trans_dist.intensity.max())
+            if forward_screen.mean() > 0:
+                index = indices[0]
+                interp_intensity = trans_dist.intensity[:index].copy()
+                interp_x = trans_dist.x[:index]
+            else:
+                index = indices[-1]
+                interp_intensity = trans_dist.intensity[index:][::-1].copy()
+                interp_x = trans_dist.x[index:][::-1]
+            if half_int is None:
+                half_int = trans_dist.intensity[index]/2
+            interp_intensity[interp_intensity < interp_intensity.max()*0.2] = 0
+            half_peak_x = np.interp(half_int, interp_intensity, interp_x)
+            return half_peak_x, half_int, index
+
+
         forward_screen = find_beam_position_dict['sim_screen']
         forward_screen.normalize()
-        indices, _ = find_peaks(forward_screen.intensity, height=0.2*forward_screen.intensity.max())
-        half_int = forward_screen.intensity[indices[0]]/2
-        half_peak_x_sim = np.interp(half_int, forward_screen.intensity[:indices[0]], forward_screen.x[:indices[0]])
+        half_peak_x_sim, half_int, index_sim = do_interp(forward_screen)
 
-        indices, _ = find_peaks(trans_dist.intensity, height=0.2*trans_dist.intensity.max())
-        half_peak_x = np.interp(half_int, trans_dist.intensity[:indices[0]], trans_dist.x[:indices[0]])
-        #print(half_int, half_peak_x_sim, half_peak_x)
+        half_peak_x, _, index_meas = do_interp(trans_dist, half_int)
 
         delta_x = -(half_peak_x - half_peak_x_sim)
         self.data['pyscan_result']['%s_axis_m' % tracker.structure.dim.lower()] += delta_x
@@ -133,6 +148,13 @@ class Xfel_data(logMsg.LogMsgBase):
             sp.plot(forward_screen.x*1e3, forward_screen.intensity)
             sp.plot(trans_dist.x*1e3, trans_dist.intensity)
             sp.plot((trans_dist.x+delta_x)*1e3, trans_dist.intensity)
+            sp.axvline(trans_dist.x[index_meas]*1e3, color='black')
+            sp.axvline(half_peak_x*1e3, color='gray')
+            sp.axvline(forward_screen.x[index_sim]*1e3, color='black')
+            sp.axvline(half_peak_x_sim*1e3, color='gray')
+        #import matplotlib.pyplot as plt
+        #plt.show()
+        #import pdb; pdb.set_trace()
 
     def get_median_sd(self):
         axis, proj, _, index = data_loader.screen_data_to_median(self.data['pyscan_result'], self.tracker.structure.dim)
@@ -153,7 +175,7 @@ class Xfel_data(logMsg.LogMsgBase):
         find_beam_position_dict = tracker.find_beam_position(tracker.beam_position, trans_dist, ref_profile)
         new_beam_position = find_beam_position_dict['beam_position']
         distance = tracker.structure_gap/2. - abs(new_beam_position)
-        self.meta_data[self.beamline+':CENTER'] = (tracker.structure_gap/2 - distance)*1e3
+        self.meta_data[self.beamline+':CENTER'] = -(tracker.structure_gap/2 - distance)*1e3
         self.tracker.meta_data = self.meta_data
         self.logMsg('Distance calibrated to %.0f um' % (distance*1e6))
         return distance

2e214cc2fa5a90db2df52918ee7c55274ee75a48
Philipp Dijkstal <philipp.dijkstal@psi.ch>
2023-10-24 16:48:51 +0200

    minor

--- plot_results.py
+++ plot_results.py
@@ -732,14 +732,14 @@ def plot_tdc_calibration(tdc_dict, image, plot_handles=None, figsize=None):
     sp_image, sp_profile, sp_screen = plot_handles
 
     blmeas_profile = tdc_dict['blmeas_profile']
-    blmeas_profile.plot_standard(sp_profile, label='Measured', center='Mean')
+    blmeas_profile.plot_standard(sp_profile, label='Input', center='Mean')
 
     image.plot_img_and_proj(sp_image, plot_gauss=False, sqrt=True)
     raw_screen = tdc_dict['meas_screen_raw']
     raw_screen.plot_standard(sp_screen, label='Measured', color='black', show_mean=True)
 
     forward_screen = tdc_dict['tdc_forward_screen']
-    forward_screen.plot_standard(sp_screen, label='Meas. forward', show_mean=True)
+    forward_screen.plot_standard(sp_screen, label='Input forward', show_mean=True)
 
     back_profile = tdc_dict['backward_dict']['profile']
     back_profile.plot_standard(sp_profile, label='Backward propagated', center='Mean', color='red')

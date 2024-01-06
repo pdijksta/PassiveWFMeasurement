5f64e106db22ebf75b1419571a48ef9b1cdd8244
Philipp <philipp.dijkstal@psi.ch>
2024-01-06 09:50:33 +0100

    initial commit of clean resolution function

--- resolution.py
+++ resolution.py
@@ -99,3 +99,113 @@ def plot_resolution(res_dict, sp_current, sp_res, max_res=20e-15):
     sp_res.set_ylim(0, max_res*1e15)
 
 
+def simulate_resolution_complete(tracker, beam, bins=(150,150), camera_res=0):
+
+    old_fo = tracker.forward_options.copy()
+    old_matrix = tracker.matrix.copy()
+    dim = tracker.structure.dim.lower()
+    other_dim = 'x' if dim == 'y' else 'y'
+
+    try:
+        tracker.forward_options['dipole_wake'] = True
+        tracker.forward_options['quad_wake'] = True
+        tracker.forward_options['long_wake'] = True
+        tracker.forward_options['long_wake_correction'] = True
+        fd_complete = tracker.forward_propagate(beam, output_details=False)
+
+        tracker.matrix[:-1,5] = 0
+        tracker.forward_options['dipole_wake'] = False
+        tracker.forward_options['long_wake'] = False
+        tracker.forward_options['long_wake_correction'] = False
+        fd_no_disp_no_dipole = tracker.forward_propagate(beam, output_details=False)
+
+        beam2 = beam.child()
+        beam2['p'] = tracker.energy_eV
+        tracker.forward_options['quad_wake'] = False
+        tracker.forward_options['long_wake_correction'] = True
+        fd_same_p0 = tracker.forward_propagate(beam2, output_details=False)
+    finally:
+        tracker.forward_options = old_fo
+        tracker.matrix = old_matrix
+
+    ## Time resolution
+    beam_t = fd_complete['beam_at_screen']['t']
+    beam_x = fd_complete['beam_at_screen'][dim]
+    if camera_res:
+        beam_x = beam_x + np.random.randn(beam_x.size)*camera_res
+    hist, xedges, yedges = np.histogram2d(beam_t, beam_x, bins=bins)
+    histsum = np.sum(hist, axis=1)
+    t_axis = t_axis0 = (xedges[1:] + xedges[:-1])/2.
+    x_axis = (yedges[1:] + yedges[:-1])/2.
+    mean_x = np.sum(hist*x_axis, axis=1) / histsum
+    dxdt = np.diff(mean_x) / np.diff(t_axis)
+    dxdt = np.append(dxdt, dxdt[-1])
+
+
+    beam_t = fd_no_disp_no_dipole['beam_at_screen']['t']
+    beam_x = fd_no_disp_no_dipole['beam_at_screen'][dim]
+    if camera_res:
+        beam_x = beam_x + np.random.randn(beam_x.size)*camera_res
+    hist, xedges, yedges = np.histogram2d(beam_t, beam_x, bins=bins)
+    histsum = np.sum(hist, axis=1)
+    t_axis = t_axis1 = (xedges[1:] + xedges[:-1])/2.
+    assert np.all(t_axis0 == t_axis1)
+    x_axis = (yedges[1:] + yedges[:-1])/2.
+
+    mean_x = np.sum(hist*x_axis, axis=1) / histsum
+    beamsize = np.sqrt(np.sum(hist*x_axis**2, axis=1) / histsum - mean_x**2)
+    time_res = beamsize/dxdt
+
+    ## Energy resolution
+    #nat beam size
+    beam_x = fd_no_disp_no_dipole['beam_at_screen'][dim]
+    beam_y = fd_no_disp_no_dipole['beam_at_screen'][other_dim]
+    if camera_res:
+        beam_x = beam_x + np.random.randn(beam_x.size)*camera_res
+        beam_y = beam_y + np.random.randn(beam_x.size)*camera_res
+    hist, xedges, yedges = np.histogram2d(beam_x, beam_y, bins=bins)
+    histsum = np.sum(hist, axis=1)
+    x_axis = (xedges[1:] + xedges[:-1])/2.
+    y_axis = (yedges[1:] + yedges[:-1])/2.
+
+    mean_y = np.sum(hist*y_axis, axis=1) / histsum
+    beamsize = np.sqrt(np.sum(hist*y_axis**2, axis=1) / histsum - mean_y**2)
+    eres1 = beamsize/tracker.disp*tracker.energy_eV
+
+    # energy chirp
+    beam_t = fd_complete['beam_at_screen']['t']
+    beam_p = fd_complete['beam_at_screen']['p']
+    hist, xedges, yedges = np.histogram2d(beam_t, beam_p, bins=bins)
+    histsum = np.sum(hist, axis=1)
+    t_axis = t_axis2 = (xedges[1:] + xedges[:-1])/2.
+    assert np.all(t_axis2 == t_axis0)
+    p_axis = (yedges[1:] + yedges[:-1])/2.
+    mean_p = np.sum(hist*p_axis, axis=1) / histsum
+    dp_dt = np.diff(mean_p)/np.diff(t_axis)
+    dp_dt = np.append(dp_dt, dp_dt[-1])
+    eres2 = time_res*dp_dt
+
+    # Induced espread
+    beam_t = fd_same_p0['beam_at_screen']['t']
+    beam_p = fd_same_p0['beam_at_screen']['p']
+    hist, xedges, yedges = np.histogram2d(beam_t, beam_p, bins=bins)
+    histsum = np.sum(hist, axis=1)
+    t_axis = t_axis3 = (xedges[1:] + xedges[:-1])/2.
+    assert np.all(t_axis3 == t_axis0)
+    p_axis = (yedges[1:] + yedges[:-1])/2.
+    mean_p = np.sum(hist*p_axis, axis=1) / histsum
+    eres3 = np.sqrt(np.sum(hist*p_axis**2, axis=1) / histsum - mean_p**2)
+
+    eres = np.sqrt(eres1**2+eres2**2+eres3**2)
+    output = {
+            'fd_no_disp_no_dipole': fd_no_disp_no_dipole,
+            'fd_complete': fd_complete,
+            'time': t_axis0,
+            'time_res': time_res,
+            'eres1': eres1,
+            'eres2': eres2,
+            'eres3': eres3,
+            'eres': eres
+            }
+    return output
+
